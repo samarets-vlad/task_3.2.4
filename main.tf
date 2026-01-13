470c2b8d8ef2f8ee60fb46bc9d48cfd2ee90e131
Vlad <v.mediocrity@gmail.com>
2026-01-13 12:31:38 +0000

    first test

---
+++ main.tf
@@ -0,0 +1,99 @@
+resource "aws_vpc" "main" {
+
+ cidr_block = "10.0.0.0/16"
+
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "route" {
+    vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "together" {
+    subnet_id = aws_subnet.main.id
+    route_table_id = aws_route_table.route.id
+}
+
+
+resource "aws_security_group" "web_sg" {
+  name        = "allow_web_traffic"
+  description = "Allow Web and SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id  
+
+  ingress {
+    description = "HTTP from Internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from Internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro" 
+
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  
+  
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+}
