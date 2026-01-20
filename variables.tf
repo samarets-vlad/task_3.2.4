5d1523607b7e40d773686eaf73706693437ff2c0
Vlad <v.mediocrity@gmail.com>
2026-01-20 21:21:49 +0000

    bestpractise3

--- variables.tf
+++ variables.tf
@@ -9,17 +9,6 @@ variable "domain_name" {
   default     = "mydevtasktrain.pp.ua" 
 }
 
-variable "enable_eip" {
-  type    = bool
-  default = false
-}
-
-variable "domain_name" {
-  description = "The domain name for the application"
-  type        = string
-  default     = "mydevtasktrain.pp.ua" 
-}
-
 variable "ssh_public_key" {
   description = "Public SSH key for EC2 instance"
   type        = string
